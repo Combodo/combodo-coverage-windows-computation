8ab0b729c72342d7ecd0cb9a748d5d980a97c0cb
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-11-19 18:44:12 +0100

    :globe_with_meridians: Update ES translations Many thanks to Miguel Turrubiates !

--- es_cr.dict.combodo-coverage-windows-computation.php
+++ es_cr.dict.combodo-coverage-windows-computation.php
@@ -1,9 +1,16 @@
 <?php
+ /**
+ * Spanish Localized data
+ *
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ * @traductor   Miguel Turrubiates <miguel_tf@yahoo.com> 
+ */
 
-//
+ //
 // Class: lnkCustomerContractToService
 //
-Dict::Add('ES CR', 'Spanish', 'Español, Castellaño', array(
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
 	'Class:lnkCustomerContractToService/Attribute:coveragewindow_id' => 'Ventana de Cobertura',
 	'Class:lnkCustomerContractToService/Attribute:coveragewindow_id+' => '',
 	'Class:lnkCustomerContractToService/Attribute:coveragewindow_name' => 'Nombre de Ventana de Cobertura',
